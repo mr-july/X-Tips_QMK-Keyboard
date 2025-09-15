a6600dbdb7ba36db34f7c7c75929badb8c3c9f2d
Igor Lubimov <mr_july@mail.ru>
2025-09-15 12:44:19 +0200

    fix: change copyright year

--- x3s/keymaps/mr-july_ozinshi/config.h
+++ x3s/keymaps/mr-july_ozinshi/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 mr. JULY <mr_july@mail.ru>
+Copyright 2025 mr. JULY <mr_july@mail.ru>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
